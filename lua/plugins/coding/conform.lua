62d1981e425785c7b78097f7154630f1dd43f2fb
panghu-huang <wokeyifrontend@gmail.com>
2024-08-30 19:46:29 +0900

    use prettier as js formatter

--- lua/plugins/coding/conform.lua
+++ lua/plugins/coding/conform.lua
@@ -5,9 +5,13 @@ return {
     local options = {
       formatters_by_ft = {
         lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        javascriptreact = { "prettier" },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 2500,
         lsp_format = 'fallback',
       },
     }
