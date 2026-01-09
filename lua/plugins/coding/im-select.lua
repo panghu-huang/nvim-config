580e88dd3ea3ecd99e2c3889000a5ff8fcb4c49a
panghu-huang <wokeyifrontend@gmail.com>
2026-01-09 10:47:36 +0900

    add im select

---
+++ lua/plugins/coding/im-select.lua
@@ -0,0 +1,25 @@
+return {
+  "keaising/im-select.nvim",
+  event = "VeryLazy",
+  config = function()
+    require('im_select').setup({
+      default_im_select       = "com.apple.keylayout.US",
+
+      -- Restore the default input method state when the following events are triggered
+      -- "VimEnter" and "FocusGained" were removed for causing problems, add it by your needs
+      set_default_events      = { "InsertLeave", "CmdlineLeave", "FocusGained" },
+
+      -- Restore the previous used input method state when the following events
+      -- are triggered, if you don't want to restore previous used im in Insert mode,
+      -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
+      -- as `set_previous_events = {}`
+      set_previous_events     = { "InsertEnter" },
+
+      -- Show notification about how to install executable binary when binary missed
+      keep_quiet_on_no_binary = false,
+
+      -- Async run `default_command` to switch IM or not
+      async_switch_im         = true
+    })
+  end,
+}
